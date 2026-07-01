36cdcf5516a90d6acf47361372b9ef57ef3b34bc
heramb <salunkheheramb@gmail.com>
2026-07-01 23:16:33 +0530

    setting up sv

---
+++ moode-os/src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<!-- main.html -->
+<!DOCTYPE html>
+<head>
+    <title>moodE OS | Say Hello to MoodE!</title>
+</head>
+
+<body
+    style="font-family: Noto Sans, system-ui, sans-serif; height: 100vh; background-image: url(OS/Assets/wall.gif); background-size: cover; background-repeat: no-repeat; margin: 0px;">
+    <div id="navbar" class="navbar"
+        style="position: absolute; width: 100%; display: flex; backdrop-filter: blur(10px); background-color: rgba(0, 0, 0, 0.125); color: #fff; justify-content: space-between; gap: 32px;">
+        <p
+            style="margin-left: 16px; font-weight: 700; background-color: rgba(256, 256, 256, 0.125); padding: 4px 12px; border-radius: 16px;">
+            moodEOS
+        </p>
+        <p
+            style="margin-left: 16px; background-color: rgba(256, 256, 256, 0.25); padding: 4px 12px; border-radius: 16px;">
+            ☀️ Keep Hustling! ☁️
+        </p>
+        <p id="timeElement"
+            style="margin-right: 16px; background-color: rgba(256, 256, 256, 0.125); padding: 4px 12px; border-radius: 16px;">
+            1/10/2025 5:00 AM
+        </p>
+    </div>
+    <div>
+        <div id="desktopApps" style="padding-top: 500px; padding-left: 16px;">
+            <div style="text-align: center; padding: 16px; filter: drop-shadow(0 0 8px black); width: fit-content">
+                <p class="icon-name">Welcome</p>
+            </div>
+            <div id="desktopApps" style="padding-top: 4px; padding-left: 0px;">
+                <div style="text-align: center; padding: 16px; filter: drop-shadow(0 0 8px black); width: fit-content">
+                    <img class="icons" src="OS/Assets/moode_OS.png" alt="welcome">
+                    <p class="icon-name">Bootloader</p>
+                </div>
+            </div>
+            <div id="welcome" class="welcome">  
+                <div id="welcomeheader"
+                class="welcomeheader">
+                <div id="welcomeclose" class="closebutton   "></div>
+                <p style="margin: 0px; color: #fff; font-weight: 500">Welcome</p>
+                
+            </div>
+    
+                    <div class="welcome-content">
+                        <img alt="moodEOS" style="width: 64px; height: 64px; border-radius: 32px; object-fit: cover"
+                            src="OS/Assets/moode_OS.png" />
+                        <h1 style="margin: 4px;">moodEOS</h1>
+                        <p style="margin: 0px;">
+                            <dfn>moodE OS</dfn> is a feature-limited <abbr title="Operating System">OS</abbr> to link your
+                            <br />
+                            <code>real-life</code> and <code class="code2">digital-life</code> together!
+                        </p>
+                    </div>
+            </div>
+        </div>
+    </div>
+</body>
